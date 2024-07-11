216e0ee1a198b1d11e201bb68bc86c89b6eb75ef
Aditya Mewada <eng.adityamewada@gmail.com>
2024-07-11 09:33:07 +0530

    Login

--- reactV2/src/App.jsx
+++ reactV2/src/App.jsx
@@ -1,1 +1,13 @@
 // app component - function 
+import Login from './LoginPage.jsx'
+import './App.css'
+
+const App =()=>{
+    return(
+        <div className='container'>
+            <Login/>
+        </div>
+    )
+}
+
+export default App;
